11906a9ff82f8c356fb98db787c05825bd6c8578
wanghj <hongjuan.wang@intel.com>
2017-05-17 15:17:52 +0800

    [test] Add automation tests for CLI - Add 19 auto tests for CLI and all tests are pass
    Signed-off-by: Hongjuan Wang <hongjuan.wang@intel.com>

---
+++ test/cmd.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let exec = require('child_process').exec,
+    path = require('path');
+
+function run(opt) {
+    let cmd = ['node', 'zephyrjs-cli.js', opt].join(' '),
+        data = {};
+    return new Promise((resolve, reject) => {
+        exec(cmd, {
+            cwd: path.dirname(path.dirname(__filename))
+        }, function(err, stdout, stderr) {
+            if (err) {
+                data['output'] = err;
+                resolve(data);
+            } else {
+                let output = stdout.split('\t')[0].split('\n');
+                data['output'] = output;
+                resolve(data);
+            }
+        })
+    });
+}
+module.exports = run;
